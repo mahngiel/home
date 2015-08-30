371707bd1e8fe6d9d255edad015866cbd243fa8e
Kris Reeck <kris.reeck@gmail.com>
2015-08-30 15:09:29 -0600

    Better apache

--- provision/apache/init.sh
+++ provision/apache/init.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y apache2 \
-    libapache2-mod-php5
+echo "deb http://us.archive.ubuntu.com/ubuntu/ trusty multiverse
+deb-src http://us.archive.ubuntu.com/ubuntu/ trusty multiverse
+deb http://us.archive.ubuntu.com/ubuntu/ trusty-updates multiverse
+deb-src http://us.archive.ubuntu.com/ubuntu/ trusty-updates multiverse" | sudo tee -a /etc/apt/sources.list
 
-sudo a2enmod rewrite headers deflate ssl
+sudo apt-get update && sudo apt-get install -y apache2 \
+    libapache2-mod-php5 \
+    libapache2-mod-fastcgi
+
+sudo a2enmod rewrite headers deflate ssl actions fastcgi alias
+sudo a2econf php5-fpm
+
+sudo a2dissite 000-default.conf
 
 sudo cp /vagrant/apache/home.conf /etc/apache2/sites-available/home.conf
 
