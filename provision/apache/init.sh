450650008504edb5801fc2ecff40d6e048bb3092
Kris Reeck <kris.reeck@gmail.com>
2017-03-07 23:18:21 -0500

    Line endings LF

--- provision/apache/init.sh
+++ provision/apache/init.sh
@@ -5,4 +5,3 @@ cp /srv/mahngiel/provision/apache/apache2.conf $APACHE_CONFDIR/
 rm $APACHE_CONFDIR/sites-enabled/*
 
 cp /srv/mahngiel/provision/apache/home.conf $APACHE_CONFDIR/sites-enabled/home.conf
-
